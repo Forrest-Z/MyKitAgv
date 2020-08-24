d459c16f4660426a23b3cb508888c57b8018b343
nhamtung <nhamtung125@gmail.com>
2020-08-24 23:41:35 +0700

    save

--- catkin_ws/devel/share/gennodejs/ros/rosbridge_library/msg/_index.js
+++ catkin_ws/devel/share/gennodejs/ros/rosbridge_library/msg/_index.js
@@ -1,24 +1,24 @@
 
 "use strict";
 
-let TestTimeArray = require('./TestTimeArray.js');
-let TestDurationArray = require('./TestDurationArray.js');
-let TestHeaderArray = require('./TestHeaderArray.js');
-let TestHeaderTwo = require('./TestHeaderTwo.js');
 let TestUInt8 = require('./TestUInt8.js');
 let TestUInt8FixedSizeArray16 = require('./TestUInt8FixedSizeArray16.js');
+let Num = require('./Num.js');
 let TestChar = require('./TestChar.js');
 let TestHeader = require('./TestHeader.js');
-let Num = require('./Num.js');
+let TestHeaderArray = require('./TestHeaderArray.js');
+let TestHeaderTwo = require('./TestHeaderTwo.js');
+let TestTimeArray = require('./TestTimeArray.js');
+let TestDurationArray = require('./TestDurationArray.js');
 
 module.exports = {
-  TestTimeArray: TestTimeArray,
-  TestDurationArray: TestDurationArray,
-  TestHeaderArray: TestHeaderArray,
-  TestHeaderTwo: TestHeaderTwo,
   TestUInt8: TestUInt8,
   TestUInt8FixedSizeArray16: TestUInt8FixedSizeArray16,
+  Num: Num,
   TestChar: TestChar,
   TestHeader: TestHeader,
-  Num: Num,
+  TestHeaderArray: TestHeaderArray,
+  TestHeaderTwo: TestHeaderTwo,
+  TestTimeArray: TestTimeArray,
+  TestDurationArray: TestDurationArray,
 };
